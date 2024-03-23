d3571104036864b3440a366d1633572fdf384f82
Enrique Aceves <enrique.j.aceves@gmail.com>
2024-03-23 14:43:24 -0700

    Updated edges variable structure from a list with sublists to a list of tuples (research suggests this is a better structure to store the edges and weights)

--- graph.py
+++ graph.py
@@ -1,29 +1,26 @@
+#function called build_graph() is created and returns 'graph' obect that can be searched
+
 #importing  dictionaries
 from collections import defaultdict
 
 #Initializeing the graph
-
 def build_graph():
-    edges = [
-         ["A", "B", 3], ["A", "E", 1], 
-        ["A", "C", 4], ["B", "D", 1],
-        ["B", "E", 2], ["C", "F", 1],
-        ["C", "G", 1], ["D", "E", 3]
-    ]
+    edges = [("A", "B", 3), ("A", "E", 1),
+             ("A", "C", 4), ("B", "D", 1),
+             ("B", "E", 2), ("C", "F", 2),
+             ("C", "G", 2), ("D", "E", 3),
+             ("E", "F", 3)]
 
-    graph = defaultdict(list)
+    graph = defaultdict(list) #storing the "edges" lsit in a dictionary which is stored as 'graph'
 
-  # Iterate over the edges. WE CAN DELETE THE FOR-LOOP WHEN WE HAVE THE BFS, AS THIS
+
+# Iterate over the edges. WE CAN DELETE THE FOR-LOOP WHEN WE HAVE THE BFS, AS THIS
   #WAS HERE JUST TO MAKE SURE THE GRAPH WAS BEING CREATED CORRECTLY 
-    for edge in edges:
-        source, dest, weight = edge
-
-        # Add the edge to the graph with the weight as the second element
+  
+    for (source, dest, weight) in edges:
         graph[source].append((dest, weight))
 
-        # Print the edge
+        # Print the edge & its weight
         print(f"{source} <--> {dest} (weight: {weight})")
 
     return graph
-
-build_graph() # Calling the function to initialize & Output Graph edges & Weights
