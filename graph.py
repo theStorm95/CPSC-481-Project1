f5d185fc2965457b68c0d36f7f1bb1c024f75f12
Nathan Storm <105747854+theStorm1995@users.noreply.github.com>
2024-03-26 22:43:29 -0700

    Fixed bug

--- graph.py
+++ graph.py
@@ -1,21 +1,21 @@
-#function called build_graph() is created and returns 'graph' obect that can be searched
+# function called build_graph() is created and returns 'graph' obect that can be searched
 
-#importing  dictionaries
+# importing  dictionaries
 from collections import defaultdict
 
-#Initializeing the graph
-def build_graph():
-    edges = [("A", "B", 3), ("A", "E", 1),
-             ("A", "C", 4), ("B", "D", 1),
-             ("B", "E", 2), ("C", "F", 2),
-             ("C", "G", 2), ("D", "E", 3),
-             ("E", "F", 3)]
+# Initializeing the graph
 
-    graph = defaultdict(list) #storing the "edges" lsit in a dictionary which is stored as 'graph'
+
+def build_graph():
+    edges = [("A", "B", 3), ("A", "C", 4), ("A", "E", 5), ("B", "A", 3), ("B", "D", 1), ("B", "E", 2), ("C", "A", 4), ("C", "F", 2), ("C", "G", 2),
+             ("D", "B", 1), ("D", "E", 3), ("E", "A", 5), ("E", "B", 2), ("E", "D", 3), ("E", "F", 3), ("F", "C", 2), ("F", "E", 3), ("G", "C", 2)]
+
+    # storing the "edges" lsit in a dictionary which is stored as 'graph'
+    graph = defaultdict(list)
 
 # Iterate over the edges. WE CAN DELETE THE FOR-LOOP WHEN WE HAVE THE BFS, AS THIS
-  #WAS HERE JUST TO MAKE SURE THE GRAPH WAS BEING CREATED CORRECTLY 
-  
+  # WAS HERE JUST TO MAKE SURE THE GRAPH WAS BEING CREATED CORRECTLY
+
     for (source, dest, weight) in edges:
         graph[source].append((dest, weight))
 
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from bfs import bfs
 from graph import build_graph
 
+
 def main():
-  graph = build_graph()
-  
-  print(graph)
-  bfs(graph, 'A', 'F')
-  
+    graph = build_graph()
+
+    print(graph)
+    bfs(graph, 'A', 'F')
+
+
 if __name__ == "__main__":
-  main()
+    main()
